745197eb8fbb6ed06005c526f5824af7f97ba09f
Dmitry Vasiliev <dima@hlabs.spb.ru>
2009-12-29 20:32:51 +0300

    Added license

--- setup.py
+++ setup.py
@@ -1,7 +1,30 @@
 #! /usr/bin/env python
-#
-# Copyright (C) 2009, dmitry Vasiliev <dima@hlabs.spb.ru>
-#
+# Copyright (c) 2009, Dmitry Vasiliev <dima@hlabs.spb.ru>
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+#  * Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#  * Neither the name of the copyright holders nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission. 
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 __author__ = "Dmitry Vasiliev <dima@hlabs.spb.ru>"
 
@@ -10,14 +33,26 @@ from setuptools import setup, find_packages
 
 setup(
     name="erlport",
-    version="0.0.2",
+    version="0.2",
     description="Erlang port protocol",
+    long_description="Erlang port protocol",
     author="Dmitry Vasiliev",
     author_email="dima@hlabs.spb.ru",
+    license="BSD",
+    platforms="any",
 
     packages=find_packages("src", exclude=["*.tests"]),
     package_dir={"": "src"},
 
     zip_safe = True,
     test_suite="erlport.tests.test_suite",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries",
+        ],
 )
