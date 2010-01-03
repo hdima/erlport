b0b876f300ca1f285c3f2989e3e601cce700b86b
Dmitry Vasiliev <dima@hlabs.spb.ru>
2010-01-03 19:26:48 +0300

    Updated README

--- src/erlport/__init__.py
+++ src/erlport/__init__.py
@@ -28,6 +28,7 @@
 """Erlang port protocol."""
 
 __author__ = "Dmitry Vasiliev <dima@hlabs.spb.ru>"
+__version__ = "0.3"
 
 from erlport.erlproto import Port, Protocol
 from erlport.erlterms import IncompleteData, Atom, String, decode, encode
