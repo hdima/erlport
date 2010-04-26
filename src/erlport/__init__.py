8e1bae7f3cb0033723c73948b123c15a07d3bad9
Dmitry Vasiliev <dima@hlabs.spb.ru>
2010-04-26 17:25:11 +0400

    Forget to update __version__

--- src/erlport/__init__.py
+++ src/erlport/__init__.py
@@ -28,7 +28,7 @@
 """Erlang port protocol."""
 
 __author__ = "Dmitry Vasiliev <dima@hlabs.spb.ru>"
-__version__ = "0.4"
+__version__ = "0.6"
 
 from erlport.erlproto import Port, Protocol
 from erlport.erlterms import IncompleteData, decode, encode
