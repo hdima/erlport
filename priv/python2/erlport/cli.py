0e886b9f9dcbbddac03e87a652771cb82112e61b
Dmitry Vasiliev <dima@hlabs.org>
2012-08-24 22:54:58 +0200

    Remove unused import

--- priv/python2/erlport/cli.py
+++ priv/python2/erlport/cli.py
@@ -25,7 +25,6 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-import sys
 from optparse import OptionParser, OptionValueError
 
 from erlport import erlang
