642b800eabdb67589adfe990123605fafaa301d4
Steve Cohen <steve@pinterest.com>
2016-02-23 14:39:45 -0800

    Used random number rather than UUID

--- priv/ruby1.8/erlport/erlang.rb
+++ priv/ruby1.8/erlport/erlang.rb
@@ -36,39 +36,6 @@ include ErlPort::ErlTerm
 require 'securerandom'
 
 
-
-module SecureRandom
-  # Backport of missing SecureRandom methods from 1.9
-  class << self
-    def method_missing(method_sym, *arguments, &block)
-      case method_sym
-      when :urlsafe_base64
-        r19_urlsafe_base64(*arguments)
-      when :uuid
-        r19_uuid(*arguments)
-      else
-        super
-      end
-    end
-
-    private
-    def r19_urlsafe_base64(n=nil, padding=false)
-      s = [random_bytes(n)].pack("m*")
-      s.delete!("\n")
-      s.tr!("+/", "-_")
-      s.delete!("=") if !padding
-      s
-    end
-
-    def r19_uuid
-      ary = random_bytes(16).unpack("NnnnnN")
-      ary[2] = (ary[2] & 0x0fff) | 0x4000
-      ary[3] = (ary[3] & 0x3fff) | 0x8000
-      "%08x-%04x-%04x-%04x-%04x%08x" % ary
-    end
-  end
-end
-
 module ErlPort
 module Erlang
 
@@ -312,7 +279,7 @@ module Erlang
 
     class MessageId
         def generate &code
-            code.call SecureRandom.uuid
+            code.call SecureRandom.random_number(2 << 128)
         end
     end
 end
